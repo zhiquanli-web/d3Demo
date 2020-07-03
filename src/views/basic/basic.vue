86fc21a67a8d2b4b51009836048e8a276b3ec23c
lizhiquan <zhiquan.li@gl-inet.com>
2020-07-03 18:25:37 +0800

    添加 圆形，正三角形，矩形，正五边形，正六边形

--- src/views/basic/basic.vue
+++ src/views/basic/basic.vue
@@ -4,35 +4,216 @@
             <div slot="header" class="clearfix">
                 <span>基础图形</span>
             </div>
-            <div id="rectbox"></div>
+            <div>
+                <div id="rectbox"></div>
+            </div>
         </el-card>
         
     </div>
 </template>
 
 <script>
+import { removeToken } from '../../utils/auth'
 export default {
   mounted() {
-      let width = 100,
-          height = 50,
-          boxWidth = 1000,
-          boxHeight = 500,
-          color = '#f60',
-          strokColor = '#0f0'
-      // 添加一个 svg 标签
-      let svg = d3.select("#rectbox")
-                  .append('svg')
-                  .attr('width', boxWidth)
-                  .attr('height', boxHeight)
-      // 创建一个矩形
-      svg.append('rect') 
-          .attr('x', 50) // 左上定点距离x轴距离
-          .attr('y', 50) // 左上定点距离y轴距离
-          .attr('width', width) // 矩形的宽度
-          .attr('height', height) // 矩形高度
-          .attr('fill', color) // 填充颜色
-          .attr('stroke', strokColor) // 边框颜色
-          .attr('size', '5px') // 边框大小
+    // 添加一个 svg 标签
+    let svg = d3.select("#rectbox")
+                .append('svg')
+                .attr('width', 1000)
+                .attr('height', 800)
+
+    var container = svg.append('g')
+    
+    // 矩形
+    this.drawRegularPolygon(container, {
+        type: 'rect',
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 50,
+        fill: '#fff',
+        stroke: '#0f0',
+        strokeWidth: 2,
+        transformRotate: 0
+    })
+
+    container.append('text')
+                .text('矩形')
+                .style('font-family', 'simsun')
+                .style('font-size', 12)
+                .style('fill', '#f60')
+                .style('font-weight', 400)
+                .attr('x', 85)
+                .attr('y', 80)
+
+    // 圆形
+    this.drawRegularPolygon(container, {
+        type: 'circle',
+        r: 25,
+        cx: 200,
+        cy: 70,
+        fill: '#fff',
+        stroke: '#0f0',
+        strokeWidth: 2 
+    })
+
+    container.append('text')
+            .text('圆形')
+            .style('font-family', 'simsun')
+            .style('font-size', 12)
+            .style('fill', '#f60')
+            .style('font-weight', 400)
+            .attr('x', 190)
+            .attr('y', 75)
+    // 正三角形
+    this.drawRegularPolygon(container, {
+        type: 'trilateral',
+        r: 25,
+        x: 280,
+        y: 80,
+        fill: '#fff',
+        stroke: '#0f0',
+        strokeWidth: 2,
+        transformRotate: 0
+    })
+    // 正五边形
+    this.drawRegularPolygon(container, {
+        type: 'pentagon',
+        r: 20,
+        x: 350,
+        y: 75,
+        fill: '#fff',
+        stroke: '#0f0',
+        strokeWidth: 2,
+        transformRotate: 0
+    })
+    // 正六边形
+    this.drawRegularPolygon(container, {
+        type: 'hexagon',
+        r: 20,
+        x: 420,
+        y: 75,
+        fill: '#fff',
+        stroke: '#0f0',
+        strokeWidth: 2,
+        transformRotate: 0
+    })
+    
+      // 五角星
+      // svg.append('polygon')
+      //     .attr('points', "100,100 190,100 115,200 145,50 175,200")
+      //     .attr('stroke', "#0f0")
+      //     .attr('stroke-width', 2)
+      //     .attr('fill', '#f60')
+          // .attr('fill-rule', 'evenodd')
+     /**
+     * 在图形内部则填充颜色
+     * fill-rule: nonzero | evenodd   
+     *    nonzero: 按该规则，要判断一个点是否在图形内，从该点作任意方向的一条射线，然后检测射线与图形路径的交点情况。从0开始计数，路径从左向右穿过射线则计数加1，从右向左穿过射线则计数减1。得出计数结果后，如果结果是0，则认为点在图形外部，否则认为在内部。
+     *    evenodd: 按该规则，要判断一个点是否在图形内，从该点作任意方向的一条射线，然后检测射线与图形路径的交点的数量。如果结果是奇数则认为点在内部，是偶数则认为点在外部
+     * 
+     * 这里的左右是根据个人所画图形线段的起始位置来区分的
+     */
+  },
+  methods: {
+      /**
+       * 
+       * @param {组标签} container 
+       * @param {节点相关数据} palygonObj
+       *  type：节点类型 circle/image/rect
+       *  src： 图片的地址(Array)
+       *  fill：填充颜色(Array)
+       *  width：节点高
+       *  height：节点宽
+       *  x：节点 x 轴坐标 (正多边形内接圆圆心x轴坐标)
+       *  y：节点 y 轴坐标 (正多边形内接圆圆心y轴坐标)
+       *  r：圆形节点半径 (正多边形内接圆半径)
+       *  cx：圆形节点圆心 x 轴坐标
+       *  cy：圆形节点圆心 y 轴坐标
+       *  transformRotate: 旋转角度
+       *  stroke: 边线
+       *  strokeWidth: 边线宽度
+       */
+    //   画正圆形、三角形、矩形、正五边形、正六边形、image   (角度 --> 弧度    弧度 = 角度 * PI / 180)
+    drawRegularPolygon(container, palygonObj) {
+        var type = palygonObj.type,
+            width = palygonObj.width,
+            height = palygonObj.height,
+            fill = palygonObj.fill,
+            stroke = palygonObj.stroke,
+            strokeWidth = palygonObj.strokeWidth,
+            x = palygonObj.x,
+            y = palygonObj.y,
+            src = palygonObj.src,
+            r = palygonObj.r,
+            cx = palygonObj.cx,
+            cy = palygonObj.cy,
+            transformRotate = palygonObj.transformRotate;
+        switch (type) {
+            case 'circle':
+                return container.append(type)
+                    .attr('r', r)
+                    .attr('cx', cx)
+                    .attr('cy', cy)
+                    .attr('fill', fill)
+                    .attr('stroke', stroke)
+                    .attr('stroke-width', strokeWidth)
+            case 'trilateral': 
+                var x1 = x + Math.sin(60 * Math.PI / 180) * r,
+                    x2 = x - Math.sin(60 * Math.PI / 180) * r,
+                    y1 = y + Math.cos(60 * Math.PI / 180) * r,
+                    y2 = y + Math.cos(60 * Math.PI / 180) * r;
+                return container.append('polygon')
+                            .attr('points', `${x},${y - r} ${x1},${y1} ${x2},${y2}`)
+                            .attr('fill', fill)
+                            .attr('stroke', stroke)
+                            .attr('stroke-width', strokeWidth)
+                            .attr('transform', `rotate(${transformRotate})`)
+            case 'rect':
+                return container.append(type)
+                    .attr('x', x) 
+                    .attr('y', y)
+                    .attr('width', width) 
+                    .attr('height', height)
+                    .attr('fill', fill) 
+                    .attr('stroke', stroke) 
+                    .attr('stroke-width', strokeWidth) 
+                    .attr('transform', `rotate(${transformRotate})`)
+            case 'pentagon':
+                var x1 = x + Math.cos(18 * Math.PI / 180) * r,
+                    x2 = x + Math.sin(36 * Math.PI / 180) * r,
+                    x3 = x - Math.sin(36 * Math.PI / 180) * r,
+                    x4 = x - Math.cos(18 * Math.PI / 180) * r,
+                    y1 = y - Math.sin(18 * Math.PI / 180) * r,
+                    y2 = y + Math.cos(36 * Math.PI / 180) * r,
+                    y3 = y + Math.cos(36 * Math.PI / 180) * r,
+                    y4 = y - Math.sin(18 * Math.PI / 180) * r;
+                return container.append('polygon')
+                            .attr('points', `${x},${y - r} ${x1},${y1} ${x2},${y2} ${x3},${y3} ${x4},${y4}`)
+                            .attr('fill', fill)
+                            .attr('stroke', stroke)
+                            .attr('stroke-width', strokeWidth)
+                            .attr('transform', `rotate(${transformRotate})`)
+            case 'hexagon':
+                var x0 = x - Math.cos(60 * Math.PI / 180) * r,
+                    x1 = x + Math.cos(60 * Math.PI / 180) * r,
+                    x2 = x + Math.sin(90 * Math.PI / 180) * r,
+                    x3 = x + Math.cos(60 * Math.PI / 180) * r,
+                    x4 = x - Math.cos(60 * Math.PI / 180) * r,
+                    x5 = x - Math.sin(90 * Math.PI / 180) * r,
+                    y0 = y - Math.sin(60 * Math.PI / 180) * r,
+                    y1 = y - Math.sin(60 * Math.PI / 180) * r,
+                    y2 = y - Math.cos(90 * Math.PI / 180) * r,
+                    y3 = y + Math.sin(60 * Math.PI / 180) * r,
+                    y4 = y + Math.sin(60 * Math.PI / 180) * r,
+                    y5 = y - Math.cos(90 * Math.PI / 180) * r;
+                return container.append('polygon')
+                            .attr('points', `${x0},${y0} ${x1},${y1} ${x2},${y2} ${x3},${y3} ${x4},${y4} ${x5},${y5}`)
+                            .attr('fill', fill)
+                            .attr('stroke', stroke)
+                            .attr('stroke-width', strokeWidth)
+        }
+    }
   }
 }
 </script>
