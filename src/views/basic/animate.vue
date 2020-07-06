7c23a0934a74923f21e1c0524cb578e7f3bad4cb
baishi <842048365@qq.com>
2020-07-06 21:31:22 +0800

    添加第一个动画

--- src/views/basic/animate.vue
+++ src/views/basic/animate.vue
@@ -25,7 +25,7 @@
 
             var container = svg.append('g')
             // 矩形
-            var rect  = container.append('rect')
+            var needle  = container.append('rect')
                                 .attr('x', 100)
                                 .attr('y', 100)
                                 .attr('width', 200)
@@ -33,11 +33,38 @@
                                 .attr('fill', '#0f0')
                                 .attr('stroke', '#000')
                                 .attr('stroke-width', 1)
-            rect.transition()
+                                .transition()
+                                .delay(3000) // 延迟3秒执行动画
+                                .duration(2000) // 动画时长
+                                .attrTween('fill', () => {
+                                    return d3.interpolateRgb('red', 'blue')   // 在 red 和 blue 之间进行插值
+                                })
+                                .ease('linear') 
+                                /**
+                                 * linear：普通的线性变化
+                                 * circle：慢慢地到达变换的最终状态
+                                 * elastic：带有弹跳的到达最终状态
+                                 * bounce：在最终状态处弹跳几次
+                                 */
+
+                        
+            /* rect.transition()
                     .attr('width', 30)
                     .delay(2000)
                     .duration(3000)
-                    .ease('blunce') // 过度样式  弹跳
+                    .ease('blunce') // 过度样式  弹跳 */
+            /* function turnNeedle(){
+                needle
+                .transition()
+                .duration(2000)
+                .attrTween("transform", tween);
+                function tween(d, i, a) {
+                    return d3.interpolateString("rotate(0, 150, 130)", "rotate(360, 150, 130)");
+                }
+            }
+            setInterval(() => {
+                turnNeedle()
+            }, 2000) */
             // 动画
             // rectDom.on('click', function() {
             //     rectDom.transition()
