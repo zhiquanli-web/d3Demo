f8cadcb689375bd6f88541cbd9ff398c421b9968
baishi-web <842048365@qq.com>
2020-07-07 21:35:42 +0800

    基础动画

--- src/views/basic/animate.vue
+++ src/views/basic/animate.vue
@@ -25,34 +25,31 @@
 
             var container = svg.append('g')
             // 矩形
-            var needle  = container.append('rect')
-                                .attr('x', 100)
-                                .attr('y', 100)
-                                .attr('width', 200)
-                                .attr('height', 100)
+            var needle  = container.append('circle')
+                                .attr('r', 30)
+                                .attr('cy', 100)
+                                .attr('cx', 100)
                                 .attr('fill', '#0f0')
                                 .attr('stroke', '#000')
                                 .attr('stroke-width', 1)
-                                .transition()
-                                .delay(3000) // 延迟3秒执行动画
-                                .duration(2000) // 动画时长
-                                .attrTween('fill', () => {
-                                    return d3.interpolateRgb('red', 'blue')   // 在 red 和 blue 之间进行插值
-                                })
-                                .ease('linear') 
-                                /**
-                                 * linear：普通的线性变化
-                                 * circle：慢慢地到达变换的最终状态
-                                 * elastic：带有弹跳的到达最终状态
-                                 * bounce：在最终状态处弹跳几次
-                                 */
-
-                        
-            /* rect.transition()
-                    .attr('width', 30)
-                    .delay(2000)
-                    .duration(3000)
-                    .ease('blunce') // 过度样式  弹跳 */
+                               
+            var run = () => {
+                needle .transition()
+                    .duration(2000) // 动画时长
+                    .attrTween('fill', () => {// 在 red 和 blue 之间进行插值
+                        return d3.interpolateRgb('red', 'blue')   
+                    })
+                    // .attr('fill', 'blue')
+                    .ease(d3.easeLinear)
+                    .on('end', run)
+            }
+            run()
+            /**
+             * linear：普通的线性变化
+             * circle：慢慢地到达变换的最终状态
+             * elastic：带有弹跳的到达最终状态
+             * bounce：在最终状态处弹跳几次
+             */
             /* function turnNeedle(){
                 needle
                 .transition()
